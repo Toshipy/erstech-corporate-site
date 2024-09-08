796ddbb5afda8a6587555b70318adc0992301b0f
Toshipy <arai1209devops@gmail.com>
2024-09-08 17:59:36 +0900

    refactor: routeの追加

---
+++ src/contants/routes.ts
@@ -0,0 +1,14 @@
+export const HOME = '/'
+export const ABOUT = '/about'
+export const SERVICE = '/service'
+export const RECRUIT = '/recruit'
+export const BLOG = '/blog'
+export const CONTACT = '/contact'
+
+export type ROUTES =
+  | typeof HOME
+  | typeof ABOUT
+  | typeof SERVICE
+  | typeof RECRUIT
+  | typeof BLOG
+  | typeof CONTACT
