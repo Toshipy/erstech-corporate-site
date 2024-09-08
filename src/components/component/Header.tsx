a6e7a0583f07cd963e030383016f5c09423c0996
Toshipy <arai1209devops@gmail.com>
2024-09-08 18:00:36 +0900

    feat: LogoとTypographyの適用

--- src/components/component/Header.tsx
+++ src/components/component/Header.tsx
@@ -1,61 +1,66 @@
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-  NavigationMenuViewport,
-} from '@/components/index'
+  Typography,
+  Logo,
+} from '@/components'
 import Link from 'next/link'
-import { Typography } from '../ui/Typography'
+
+import { HOME } from '@/contants'
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4">
+    <header className="flex items-center justify-between p-4 border-b-2">
+      <div className="flex items-center">
+        <Link href={`${HOME}`}>
+          <Logo height={25} width={116} />
+        </Link>
+        <Typography variant="4xl">Erstech</Typography>
+      </div>
       <NavigationMenu className="flex space-x-4">
         <NavigationMenuList>
           <NavigationMenuItem>
             <Link href="/" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>Home</Typography>
+                <Typography variant="2xl">Home</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="/about" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>About</Typography>
+                <Typography variant="2xl">About</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="/service" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>Service</Typography>
+                <Typography variant="2xl">Service</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="/recruit" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>Recruit</Typography>
+                <Typography variant="2xl">Recruit</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="/blog" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>Blog</Typography>
+                <Typography variant="2xl">Blog</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <Link href="/contact" legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                <Typography>Contact</Typography>
+                <Typography variant="2xl">Contact</Typography>
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
