bb8cb5a4433a50192823dd18cf6a06f63df75a7c
Toshipy <arai1209devops@gmail.com>
2024-09-21 19:37:43 +0900

    feat: product pageと product details pageの作成

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { Footer, Header, ProductDetails } from '@/components'
+
+const ProductDetail = () => {
+  return (
+    <>
+      <Header />
+      <ProductDetails />
+      <Footer />
+    </>
+  )
+}
+
+export default ProductDetail
---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Header, Footer, Products } from '@/components'
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <>
+      <Header />
+      <Products />
+      <Footer />
+    </>
+  )
+}
+
+export default ProductPage
