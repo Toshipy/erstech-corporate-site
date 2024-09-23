5df9db855db9e79be733d94cf8e5954208d32ae9
Toshipy <arai1209devops@gmail.com>
2024-09-23 23:33:03 +0900

    fix: footer のスタイル修正

--- src/components/component/Footer/Footer.tsx
+++ src/components/component/Footer/Footer.tsx
@@ -7,41 +7,51 @@ import { Button, Typography } from '@/components'
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="top-full flex w-full justify-between border-t-2 p-4">
-      <div>
+    <footer className="flex w-full border-t-2 bg-slate-800 px-10 py-4">
+      <div className="px-10">
         <Link href={`${HOME}`}>
-          <Image src="/daiana_logo.png" alt="logo" width={200} height={200} />
+          <Image src="/erstech_logo.png" alt="logo" width={150} height={150} />
         </Link>
-        <p>&copy; Erstech Corporation. All rigths reserved.</p>
+        <Typography className="text-white">
+          &copy; Erstech Corporation. All rigths reserved.
+        </Typography>
       </div>
-      <div className="flex flex-col items-center space-y-2">
-        <Typography variant="xl">MENU</Typography>
-        <ul className="space-y-2">
-          {ERSTECH_ROUTES.map((route) => (
-            <li key={route.href}>
-              <Button variant="ghost">
-                <Link href={route.href}>
-                  <Typography>{route.label}</Typography>
-                </Link>
-              </Button>
+      <div className="flex justify-between px-10">
+        {/* <div className="flex-col items-center space-y-2">
+          <Typography variant="2xl" className="font-serif text-white">
+            MENU
+          </Typography>
+          <ul className="space-y-2">
+            {ERSTECH_ROUTES.map((route) => (
+              <li key={route.href}>
+                <Button variant="ghost">
+                  <Link href={route.href}>
+                    <Typography className="font-serif text-white">
+                      {route.label}
+                    </Typography>
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div> */}
+        <div className="flex flex-col items-center space-y-2">
+          <Typography variant="2xl" className="font-serif text-white">
+            SOCIAL
+          </Typography>
+          <ul className="flex flex-col space-y-2">
+            <li>
+              <Link href="https://github.com/{GitHubName}">
+                <i className="ri-github-fill text-4xl text-white"></i>
+              </Link>
             </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex flex-col items-center space-y-2">
-        <Typography variant="xl">SOCIAL</Typography>
-        <ul className="flex flex-col space-y-2">
-          <li>
-            <Link href="https://github.com/{GitHubName}">
-              <i className="ri-github-fill text-4xl"></i>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://twitter.com/">
-              <i className="ri-twitter-fill text-4xl"></i>
-            </Link>
-          </li>
-        </ul>
+            <li>
+              <Link href="https://twitter.com/">
+                <i className="ri-twitter-fill text-4xl text-white"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   )
