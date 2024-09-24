75229998aa86e49ca7eb7bdcc60ae3fcd1b38873
Toshipy <arai1209devops@gmail.com>
2024-09-24 21:54:13 +0900

    refactor: styleの調整

--- src/components/component/Header/Header.tsx
+++ src/components/component/Header/Header.tsx
@@ -10,12 +10,12 @@ import {
   Logo,
 } from '@/components'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
+import { useSound } from './useSound'
 
 import { ERSTECH_ROUTES, HOME } from '@/contants'
-
-// import { ModeToggle } from '@/components'
+import { Volume2, VolumeX } from 'lucide-react'
 
 export const Header = () => {
   const initialValue = 100
@@ -42,6 +42,8 @@ export const Header = () => {
     () => scrollY.onChange((latest) => setIsPastThreshold(latest > thresholdY)),
     [],
   )
+
+  const { isPlaying, toggle, audioRef } = useSound()
 
   return (
     <div className="mb-20">
@@ -82,8 +84,36 @@ export const Header = () => {
                     </Link>
                   </NavigationMenuItem>
                 ))}
+                <audio ref={audioRef} loop>
+                  <source
+                    src={
+                      'https://erstech-corporate-site.s3.ap-northeast-1.amazonaws.com/sound/universe.mp3'
+                    }
+                    type="audio/mpeg"
+                  />
+                  あなたのブラウザではオーディオがサポートされていません。
+                </audio>
+                <motion.button
+                  onClick={toggle}
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  transition={{ delay: 1 }}
+                  className="xs:w-14 xs:h-14 xs:p-4 custom-bg z-50 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full p-2.5 text-foreground"
+                  aria-label={'home'}
+                >
+                  {isPlaying ? (
+                    <Volume2
+                      className="h-full w-full text-foreground group-hover:text-accent"
+                      strokeWidth={1.5}
+                    />
+                  ) : (
+                    <VolumeX
+                      className="h-full w-full text-foreground group-hover:text-accent"
+                      strokeWidth={1.5}
+                    />
+                  )}
+                </motion.button>
               </NavigationMenuList>
-              {/* <div className="ml-auto mr-5"><ModeToggle /></div> */}
             </NavigationMenu>
           </header>
         </motion.div>
